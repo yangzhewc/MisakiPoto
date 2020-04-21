65ad0b697e9a048ff6eea1ee0ca807b3352792c5
nimotaku <58283455+nimotaku@users.noreply.github.com>
2020-04-21 14:35:34 +0900

    Fadeアニメーション実装(EASY+、EASYのみ)

---
+++ NewM_Prot/Assets/Fade/FadeIntest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeIntest : MonoBehaviour
+{
+
+    public Fade fade;       //FadeCanvas取得
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        { 
+            //アニメーションを掛けてシーン遷移する
+            fade.FadeIn(1f, () =>
+            {
+                SceneManager.LoadScene("EASY");
+            });
+        }
+       
+    }
+}
